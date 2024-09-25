24cad8af01cd5d9f5148319a622956d76ab14a4e
Hakurunai <guillaumeal.pro@gmail.com>
2024-09-25 15:06:17 +0200

    End of reflection scene

--- Assets/Scripts/VectorsAndDotProduct/Laser.cs
+++ Assets/Scripts/VectorsAndDotProduct/Laser.cs
@@ -2,19 +2,39 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    [SerializeField, Range(0.5f, 10.0f)] float LaserRange = 10.0f;
+    [SerializeField, Range(2f, 20.0f)] float LaserRange = 10.0f;
+    [SerializeField, Range(1, 10)] int MaxBounces = 10;
 
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        Vector3 direction = transform.right;
         Gizmos.color = Color.red;
-
+        Vector3 direction = transform.right;
         Ray ray = new Ray(transform.position, direction);
-        if (Physics.Raycast(ray, out RaycastHit hit, LaserRange))
+
+        for (int i = 0; i < MaxBounces; ++i)
         {
-            Gizmos.DrawLine(transform.position, hit.point);
+            if (Physics.Raycast(ray, out RaycastHit hit, LaserRange))
+            {
+                Gizmos.DrawLine(ray.origin, hit.point);
+
+                Vector3 reflected = Reflect(ray.direction, hit.normal);
+                Gizmos.DrawLine(hit.point, hit.point + reflected);
+
+                //new ray for next reflection
+                ray.direction = reflected;
+                ray.origin = hit.point;
+            }
+            else break;
         }
+
+    }
+
+    Vector3 Reflect(Vector3 inDirection, Vector3 normalSurface)
+    {
+        //r = dir - 2(dir DOT normal) * normal -> vectoriel projection of our reflection
+        float proj = Vector3.Dot(inDirection, normalSurface);
+        return inDirection - 2 * proj * normalSurface;
     }
 #endif
 }
